30b0f02cc54e03cd6513ed0eae354d9d54ea1225
E. Dronkert <ednl@users.noreply.github.com>
2022-12-02 16:56:06 +0100

    MUCH easier

--- 02.c
+++ 02.c
@@ -1,61 +1,33 @@
-#include <stdio.h>  // fopen, fclose, fscanf, printf
-#include <stdbool.h>
+#include <stdio.h>
 
-#define N (2500)  // my input has 2500 lines
-static char play[N][2];
-
-static bool read(void)
-{
-    FILE *f = fopen("input02.txt", "r");
-    if (!f)
-        return false;
-    int i = 0;
-    char h0, h1;
-    while (i < N && fscanf(f, "%c %c ", &h0, &h1) == 2) {
-        play[i][0] = h0;
-        play[i][1] = h1;
-        i++;
-    }
-    fclose(f);
-    if (i != N)
-        return false;
-    return true;
-}
-
-static int part1(void)
-{
-    int score = 0;
-    for (int i = 0; i < N; ++i) {
-        const char h0 = play[i][0] - 'A';  // 0=rock, 1=paper, 2=scissors
-        const char h1 = play[i][1] - 'X';
-        switch ((h1 - h0 + 3) % 3) {
-            case 0: score += h1;     break;  // h1-h0= 0: draw    (AA, BB, CC)
-            case 1: score += h1 + 3; break;  // h1-h0=+1: h1 wins (AB, BC, CA)
-            case 2: score += h1 - 3; break;  // h1-h0=-1: h0 wins (AC, BA, CB)
-        }
-    }
-    return score + N * 4;  // compensate for score minus 4
-}
-
-static int part2(void)
-{
-    int score = 0;
-    for (int i = 0; i < N; ++i) {
-        const char h0 = play[i][0] - 'A';  // 0=rock, 1=paper, 2=scissors
-        switch (play[i][1]) {
-            case 'X': score += (h0 + 2) % 3 - 3; break;  // X: h0 wins (AC, BA, CB)
-            case 'Y': score +=  h0;              break;  // Y: draw    (AA, BB, CC)
-            case 'Z': score += (h0 + 1) % 3 + 3; break;  // Z: h1 wins (AB, BC, CA)
-        }
-    }
-    return score + N * 4;  // compensate for score minus 4
-}
+#define AX ('A' << 8 | 'X')
+#define AY ('A' << 8 | 'Y')
+#define AZ ('A' << 8 | 'Z')
+#define BX ('B' << 8 | 'X')
+#define BY ('B' << 8 | 'Y')
+#define BZ ('B' << 8 | 'Z')
+#define CX ('C' << 8 | 'X')
+#define CY ('C' << 8 | 'Y')
+#define CZ ('C' << 8 | 'Z')
 
 int main(void)
 {
-    if (!read())
-        return 1;
-    printf("Part 1: %d\n", part1());
-    printf("Part 2: %d\n", part2());
+    char buf[5];
+    int s = 0, t = 0;
+    FILE *f = fopen("input02.txt", "r");
+    while (fgets(buf, sizeof buf, f) != NULL)
+        switch (buf[0] << 8 | buf[2]) {
+            case AX: s += 4; t += 3; break;  // 1: AA = draw 1+3, 2: AC = loss 3+0
+            case AY: s += 8; t += 4; break;  // 1: AB = win  2+6, 2: AA = draw 1+3
+            case AZ: s += 3; t += 8; break;  // 1: AC = loss 3+0, 2: AB = win  2+6
+            case BX: s += 1; t += 1; break;  // 1: BA = loss 1+0, 2: BA = loss 1+0
+            case BY: s += 5; t += 5; break;  // 1: BB = draw 2+3, 2: BB = draw 2+3
+            case BZ: s += 9; t += 9; break;  // 1: BC = win  3+6, 2: BC = win  3+6
+            case CX: s += 7; t += 2; break;  // 1: CA = win  1+6, 2: CB = loss 2+0
+            case CY: s += 2; t += 6; break;  // 1: CB = loss 2+0, 2: CC = draw 3+3
+            case CZ: s += 6; t += 7; break;  // 1: CC = draw 3+3, 2: CA = win  1+6
+        }
+    fclose(f);
+    printf("%d %d\n", s, t);  // 11767 13886
     return 0;
 }
