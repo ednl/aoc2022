a7dad864d318bfe525d38f0910dfb6b7eab2dee2
E. Dronkert <ednl@users.noreply.github.com>
2022-12-11 05:46:28 +0100

    shorter & faster

--- 10.c
+++ 10.c
@@ -15,20 +15,20 @@
 int main(void)
 {
     starttimer();
-    FILE *f = fopen("input10.txt", "r");      // or: example10.txt
-    char buf[5];                              // opcodes are length 4 + 1 string terminator \0
-    int cycle = HALF, regX = 1, sigsum = 0, pixel = 0, beam;
-    while (fscanf(f, "%4s ", buf) == 1) {
-        if (!(++cycle % FULL))                // 1 tick for every word => noop=1, addx=2
-            sigsum += (cycle - HALF) * regX;  // clock=20,60,100,140,180,220
-        if (!(beam = pixel++ % FULL))         // horizontal position of the pixel beam
-            printf("\n");                     // start with blank line
+    FILE *f = fopen("input10.txt", "r");          // or: example10.txt
+    char buf[5];                                  // opcodes and operands are length <= 4, +\0
+    int clock = 0, regX = 1, sigsum = 0, beam;    // beam = horizontal position
+    while (fscanf(f, "%4s ", buf) == 1) {         // 1 clock tick for every word (so, 2 for addx)
+        if ((beam = clock++ % FULL) == HALF - 1)  // beam 19 => clock 20
+            sigsum += clock * regX;               // clock = 20,60,100,140,180,220th cycle
+        if (!beam)                                // start with blank line
+            printf("\n");
         printf("%c", abs(beam - regX) <= 1 ? '#' : ' ');  // part 2: EFUGLPAP
-        if (!(buf[0] & (1 << 6)))             // ch < 64 for numbers (also negative sign)
-            regX += atoi(buf);                // only update after complete instruction cycle
+        if (!(buf[0] & (1 << 6)))                 // ascii < 64 for numbers (including negative sign)
+            regX += atoi(buf);                    // only update after complete instruction cycle
     }
     fclose(f);
-    printf("\n\nPart 1: %d\n", sigsum);       // part 1: ex=13140, inp=15020
+    printf("\n\nPart 1: %d\n", sigsum);           // part 1: ex=13140, inp=15020
     printf("Time: %.0f us\n", stoptimer_us());
     return 0;
 }
