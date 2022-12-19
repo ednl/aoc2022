3a880c7549815e7f72aa69382c3ea734887bc672
E. Dronkert <ednl@users.noreply.github.com>
2022-12-19 15:01:16 +0100

    input of day 19

--- 17.c
+++ 17.c
@@ -6,7 +6,7 @@
  *
  * Benchmark with the internal timer on a Mac Mini M1 using this Bash oneliner:
  *   m=50000;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
- * gives a shortest runtime for my input file (not the example) of 232 µs.
+ * gives a shortest runtime for my input file (not the example) of [TBD] µs.
  * On a Raspberry Pi 4 with the CPU in performance mode: [TBD] µs.
  *   echo performance | sudo tee /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
  *   /boot/config.txt: arm_boost=1, no overclock
